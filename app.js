baf3ade5d2e766c2d29e86631f4e871ae55f85ae
Tristan Munnik <tristanmunnik3@gmail.com>
2026-06-13 13:53:30 +0200

    added search function with api fetch

---
+++ app.js
@@ -0,0 +1,45 @@
+const searchBtn = document.getElementById("searchBtn");
+
+const searchInput = document.getElementById("searchInput");
+
+const resultsDiv = document.getElementById("results");
+
+searchBtn.addEventListener('click', function () {
+    const searchTerm = searchInput.value;
+
+    if(searchTerm === '') {
+        alert("please enter a movie name");
+        return;
+    } 
+
+    const apiKey = "bdc3f806";
+    const url = `http://www.omdbapi.com/?s=${searchTerm}&apikey=${apiKey}`;
+
+    fetch(url)
+        .then(function (respone) {
+            return respone.json();
+        })
+        .then(function(data) {
+            
+            const movies = data.Search;
+
+            resultsDiv.innerHTML = '';
+
+            movies.forEach(function(movie) {
+                const movieCard = document.createElement('div');
+                movieCard.className = 'movie-card';
+
+                movieCard.innerHTML = `
+                    <img src="${movie.Poster}" alt="${movie.Title}">
+                    <h3>${movie.Title}</h3>
+                    <p>${movie.Year}</p>
+                `;
+                resultsDiv.appendChild(movieCard);
+            });
+
+        })
+        .catch(function(error) {
+            console.log('Error: ', error);
+        })
+
+});
